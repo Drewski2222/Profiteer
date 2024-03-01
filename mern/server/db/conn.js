f9193dc88e518962f36983d591c52439d3ac799b
GrossTrevor <grosstrevor000@gmail.com>
2024-03-01 09:31:14 -0500

    commented out code trying to get mongo to connect

--- mern/server/db/conn.js
+++ mern/server/db/conn.js
@@ -1,26 +1,59 @@
 const { MongoClient } = require("mongodb");
-const Db = process.env.ATLAS_URI;
-const client = new MongoClient(Db, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const uri = process.env.ATLAS_URI;
+const client = new MongoClient(uri);
  
 var _db;
  
 module.exports = {
   connectToServer: function (callback) {
-    client.connect(function (err, db) {
-      // Verify we got a good "db" object
-      if (db)
-      {
-        _db = db.db("employees");
-        console.log("Successfully connected to MongoDB."); 
+    async function run() {
+      try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        
+        // Send a ping to confirm a successful connection
+        await client.db("appdata").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        const findResult = client.db("appdata").collection("userInfo").find({
+          first_name: "Johnny"
+        });
+        for await (const doc of findResult) {
+          console.log(doc);
+        }
+
+      } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
       }
-      return callback(err);
-         });
+    }
+    // client.connect(function (err, db) {
+    //   // Verify we got a good "db" object
+    //   if (db)
+    //   {
+    //     _db = db.db("appdata");
+    //     console.log("Successfully connected to MongoDB."); 
+    //   }
+    //   return callback(err);
+    //      });
   },
  
   getDb: function () {
     return _db;
   },
 };
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("appdata").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
--- mern/server/server.js
+++ mern/server/server.js
@@ -1,13 +1,8 @@
-require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
 require("dotenv").config({ path: "./config.env" });
-
 const port = process.env.PORT || 5000;
-
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
@@ -21,46 +16,45 @@ app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
 
-// Set up the server
 
-app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(express.static("./public"));
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+// // Set up the server
 
-const server = app.listen(APP_PORT, function () {
-  console.log(`Server is up and running at http://localhost:${APP_PORT}/`);
-});
+// app.use(cookieParser());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+// app.use(express.static("./public"));
 
-const usersRouter = require("./routes/users");
-const linkTokenRouter = require("./routes/tokens");
-const bankRouter = require("./routes/banks");
-const { router: transactionsRouter } = require("./routes/transactions");
-const debugRouter = require("./routes/debug");
-const { getWebhookServer } = require("./webhookServer");
+// const usersRouter = require("./routes/users");
+// const linkTokenRouter = require("./routes/tokens");
+// const bankRouter = require("./routes/banks");
+// const { router: transactionsRouter } = require("./routes/transactions");
+// const debugRouter = require("./routes/debug");
+// const { getWebhookServer } = require("./webhookServer");
 
-app.use("/server/users", usersRouter);
-app.use("/server/tokens", linkTokenRouter);
-app.use("/server/banks", bankRouter);
-app.use("/server/transactions", transactionsRouter);
-app.use("/server/debug", debugRouter);
+// app.use("/server/users", usersRouter);
+// app.use("/server/tokens", linkTokenRouter);
+// app.use("/server/banks", bankRouter);
+// app.use("/server/transactions", transactionsRouter);
+// app.use("/server/debug", debugRouter);
 
-/* Add in some basic error handling so our server doesn't crash if we run into
- * an error.
- */
-const errorHandler = function (err, req, res, next) {
-  console.error(`Your error:`);
-  console.error(err);
-  if (err.response?.data != null) {
-    res.status(500).send(err.response.data);
-  } else {
-    res.status(500).send({
-      error_code: "OTHER_ERROR",
-      error_message: "I got some other message on the server.",
-    });
-  }
-};
-app.use(errorHandler);
+// /* Add in some basic error handling so our server doesn't crash if we run into
+//  * an error.
+//  */
+// const errorHandler = function (err, req, res, next) {
+//   console.error(`Your error:`);
+//   console.error(err);
+//   if (err.response?.data != null) {
+//     res.status(500).send(err.response.data);
+//   } else {
+//     res.status(500).send({
+//       error_code: "OTHER_ERROR",
+//       error_message: "I got some other message on the server.",
+//     });
+//   }
+// };
+// app.use(errorHandler);
 
-// Initialize our webhook server, too.
-const webhookServer = getWebhookServer();
+// // Initialize our webhook server, too.
+// const webhookServer = getWebhookServer();
