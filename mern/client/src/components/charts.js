5dcd76021351212de56098c5f089c7774e2c0b73
Drew Reisner <drewreisner@ufl.edu>
2024-04-16 22:30:04 -0400

    Added code for line graph

--- mern/client/src/components/charts.js
+++ mern/client/src/components/charts.js
@@ -1,6 +1,6 @@
 import * as d3 from 'd3'
 
-const renderDonutChart = (data, containerSelector) => {
+export const renderDonutChart = (data, containerSelector) => {
     // Set up the dimensions and radius
     const width = 300;
     const height = 300;
@@ -75,4 +75,58 @@ const renderDonutChart = (data, containerSelector) => {
         .style("fill", "black");
 };
 
-export default renderDonutChart;
+export const renderLineChart = (data, containerSelector) => {
+    // Declare the chart dimensions and margins.
+    const width = 300;
+    const height = 100;
+    const marginTop = 5;
+    const marginRight = 5;
+    const marginBottom = 5;
+    const marginLeft = 5;
+  
+    // Declare the x (horizontal position) scale.
+    const x = d3.scaleUtc(d3.extent(data, d => d.date), [marginLeft, width - marginRight]);
+  
+    // Declare the y (vertical position) scale.
+    const y = d3.scaleLinear([0, d3.max(data, d => d.close)], [height - marginBottom, marginTop]);
+  
+    // Declare the line generator.
+    const line = d3.line()
+        .x(d => x(d.date))
+        .y(d => y(d.close));
+  
+    // Create SVG element
+    const svg = d3.select(containerSelector)
+        .attr("width", width)
+        .attr("height", height)
+        .attr("viewBox", [0, 0, width, height])
+        .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
+  
+    // Add the x-axis.
+    svg.append("g")
+        .attr("transform", `translate(0,${height - marginBottom})`)
+        .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0));
+  
+    // Add the y-axis, remove the domain line, add grid lines and a label.
+    svg.append("g")
+        .attr("transform", `translate(${marginLeft},0)`)
+        .call(d3.axisLeft(y).ticks(height / 40))
+        .call(g => g.select(".domain").remove())
+        .call(g => g.selectAll(".tick line").clone()
+            .attr("x2", width - marginLeft - marginRight)
+            .attr("stroke-opacity", 0.1))
+        .call(g => g.append("text")
+            .attr("x", -marginLeft)
+            .attr("y", 10)
+            .attr("fill", "currentColor")
+            .attr("text-anchor", "start")
+            .text("↑ Daily close ($)"));
+  
+    // Append a path for the line.
+    svg.append("path")
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .attr("d", line(data));
+  
+  }
